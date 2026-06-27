3357bb9341dcf552a5bc64dd3f1dded953b1924d
Pooja-Chinthakayala <118978603+Pooja-Chinthakayala@users.noreply.github.com>
2026-06-27 18:32:28 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,32 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variables
+print(3+8)
+11
+a=10
+print(a)
+10
+b=20
+print(b)
+20
+x=40
+print(X)
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    print(X)
+NameError: name 'X' is not defined. Did you mean: 'x'?
+print(x)
+40
+>>> z=50
+>>> print(z)
+50
+>>> 3=80
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+>>> a3=80
+>>> print(a3)
+80
+>>> 6a=90
+SyntaxError: invalid decimal literal
+>>> b123456789=300
+>>> print(b123456789)
+300
